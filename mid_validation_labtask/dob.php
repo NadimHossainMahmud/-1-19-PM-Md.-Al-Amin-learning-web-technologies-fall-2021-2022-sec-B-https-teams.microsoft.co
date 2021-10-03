d8bc34414c339d06630b2d9f54bef4664690430e
nadim <nadim98hossain@gmail.com>
2021-10-03 12:36:03 +0600

    done

---
+++ mid_validation_labtask/dob.php
@@ -0,0 +1,32 @@
+<?php
+
+	if(isset($_POST['submit']))
+    {
+
+		$dob = $_POST['dob'];
+
+		if($dob == "")
+        {
+			echo "Null submission";
+		}else{
+			echo "Date of Birth: ",$dob;
+		}
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DOB Input</title>
+</head>
+<body>
+	<form method="post" action="#">
+		<fieldset>
+			<legend>Date of Birth</legend>
+			<input type="date" name="dob" value=""> <br>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+	
+</body>
+</html>
